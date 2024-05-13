1855de3d37d7ecd39a29ddc10245e4ed58fea5e5
Suguru Miyata <suguru1114@gmail.com>
2024-05-13 22:13:23 +1000

    add button click to check time

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
-import React from "react";
-
+import React, { useState } from "react";
 function App() {
+
+  const [time, setTime] = useState("");
+  const getTime = () => {
+    let currentTime = new Date().toLocaleTimeString();
+    setTime(currentTime);
+  };
+
   return (
     <div className="container">
       <h1>TIME</h1>
-      <button>Get Time</button>
+      <p>{time}</p>
+      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
