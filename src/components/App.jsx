5d1d3070b400dbfa3bcd8e8a2b468d0905038734
Suguru Miyata <suguru1114@gmail.com>
2024-05-18 22:52:07 +1000

    add real time clock

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 function App() {
 
   const [time, setTime] = useState("");
@@ -7,9 +7,16 @@ function App() {
     setTime(currentTime);
   };
 
+  useEffect(function() {
+    const intervalId = setInterval(getTime, 1000);
+    return function() {
+      clearInterval(intervalId);
+    };
+  }, []); // Empty dependency array ensures that useEffect runs only once after initial render
+
   return (
     <div className="container">
-      <h1>TIME</h1>
+      <h1>Time</h1>
       <p>{time}</p>
       <button onClick={getTime}>Get Time</button>
     </div>
